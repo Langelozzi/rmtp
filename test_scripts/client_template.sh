4b4438a06cffba129473640acbfbf09b1a6f9b9f
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2025-12-01 23:45:55 -0800

    Uncomment log server config for testing

--- test_scripts/client_template.sh
+++ test_scripts/client_template.sh
@@ -2,6 +2,6 @@
     --target-ip 192.168.1.79 \
     --target-port 3002 \
     --max-retries 3 \
-    --timeout 1
-# --log-server-ip 0.0.0.0 \
-# --log-server-port 9001 \
+    --timeout 1 \
+    --log-server-ip 192.168.1.79 \
+    --log-server-port 9001
--- test_scripts/server_template.sh
+++ test_scripts/server_template.sh
@@ -1,5 +1,5 @@
 ../build/server \
     --listen-ip 192.168.1.75 \
-    --listen-port 3003
-# --log-server-ip 0.0.0.0 \
-# --log-server-port 9001
+    --listen-port 3003 \
+    --log-server-ip 192.168.1.79 \
+    --log-server-port 9001
